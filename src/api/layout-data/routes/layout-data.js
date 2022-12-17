5241590816d8bac06d52e641a3e191862d98a410
Wandole <mchirez@gmail.com>
2022-12-17 17:19:58 +0100

    Add content-type and content to CMS for Home and layout

---
+++ src/api/layout-data/routes/layout-data.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * layout-data router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::layout-data.layout-data');
---
+++ src/api/layout-data/services/layout-data.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * layout-data service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::layout-data.layout-data');
