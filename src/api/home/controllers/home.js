9042ef74efec278ba87959149d980bb21c36395c
Wandole <mchirez@gmail.com>
2022-12-17 17:45:18 +0100

    Add custom router for home

--- src/api/home/controllers/home.js
+++ src/api/home/controllers/home.js
@@ -1,9 +1,32 @@
-'use strict';
+"use strict";
 
 /**
  * home controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::home.home');
+module.exports = createCoreController("api::home.home", ({ strapi }) => ({
+  async find(ctx) {
+    const entity = await strapi.service("api::home.home").find({
+      populate: [
+        "heroProduct.image.mobile",
+        "heroProduct.image.desktop",
+        "heroProduct.image.tablet",
+        "product1.image.mobile",
+        "product1.image.desktop",
+        "product1.image.tablet",
+        "product2.image.mobile",
+        "product2.image.desktop",
+        "product2.image.tablet",
+        "product3.image.mobile",
+        "product3.image.desktop",
+        "product3.image.tablet",
+      ],
+    });
+
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
