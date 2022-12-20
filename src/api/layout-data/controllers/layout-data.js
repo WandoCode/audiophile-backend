e373e562a43cf57a6d734a91d8c75719e32e4a51
Wandole <mchirez@gmail.com>
2022-12-20 19:29:49 +0100

    Add missing fields for layout image and text. Add custom controller to send image details

--- src/api/layout-data/controllers/layout-data.js
+++ src/api/layout-data/controllers/layout-data.js
@@ -1,9 +1,29 @@
-'use strict';
+"use strict";
 
 /**
  * layout-data controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::layout-data.layout-data');
+module.exports = createCoreController(
+  "api::layout-data.layout-data",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const entity = await strapi.service("api::layout-data.layout-data").find({
+        populate: [
+          "mainDescriptionImages.mobile",
+          "mainDescriptionImages.tablet",
+          "mainDescriptionImages.desktop",
+          "image1",
+          "image2",
+          "image3",
+        ],
+      });
+
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(sanitizedEntity);
+    },
+  })
+);
