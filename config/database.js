eaa5d9e6bc212245d95570dc81294ae26e7e7cd5
Wandole <mchirez@gmail.com>
2023-01-10 18:40:32 +0100

    Cleaned setup for postgresql database connection

--- config/database.js
+++ config/database.js
@@ -2,26 +2,14 @@ const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: "sqlite",
     connection: {
-      host: env("PGHOST", "127.0.0.1"),
-      port: env.int("PGPORT", 5931),
-      database: env("PGDATABASE", "railway"),
-      user: env("PGUSER", "postgres"),
-      password: env("PGPASSWORD", "pwd"),
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
     },
+    useNullAsDefault: true,
   },
 });
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: "sqlite",
-//     connection: {
-//       filename: path.join(
-//         __dirname,
-//         "..",
-//         env("DATABASE_FILENAME", ".tmp/data.db")
-//       ),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
