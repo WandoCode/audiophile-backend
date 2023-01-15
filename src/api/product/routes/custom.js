37496056037c5c4b45fa952035e53ac70d544143
Wandole <mchirez@gmail.com>
2023-01-15 13:35:58 +0100

    Update api routes

--- src/api/product/routes/custom.js
+++ src/api/product/routes/custom.js
@@ -2,7 +2,12 @@ module.exports = {
   routes: [
     {
       method: "GET",
-      path: "/products/:slug",
+      path: "/products/slugs",
+      handler: "product.findAll",
+    },
+    {
+      method: "GET",
+      path: "/products/product/:slug",
       handler: "product.findOne",
     },
   ],
